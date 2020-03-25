5a65d277e840efb8bdfc891378ace3ed4be8f4c8
pospisil98 <pospivo1@fel.cvut.cz>
2020-03-25 14:13:46 +0100

    Created new targets and tested recognition

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -14,18 +14,53 @@ import {
 
 import Globe from './components/Globe'
 
-export default class HelloWorldSceneAR extends Component {
+const targetNames = ["africa", "atlantic", "australia", "china", "europe",
+  "hawaii", "indonesia", "northAmerica", "southAmerica"];
 
+const angles = {
+  "africa": 15,
+  "atlantic": 340,
+  "australia": 135,
+  "china": 90,
+  "europe": 15,
+  "hawaii": 195,
+  "indonesia": 140,
+  "northAmerica": 260,
+  "southAmerica": 300,
+}
+
+
+export default class HelloWorldSceneAR extends Component {
   constructor() {
     super();
 
     // Set initial state here
     this.state = {
-      text : "Initializing AR..."
+      text: "Initializing AR...",
     };
+
+    this.tracking = [];
+    this.lastKnownPose = [];
 
     // bind 'this' to functions
     this._onInitialized = this._onInitialized.bind(this);
+  }
+
+  renderTargets = () => {
+    let targets = [];
+
+    targets = targetNames.map((name) => {
+      return (
+        <ViroARImageMarker
+          key={name}
+          target={name}        
+          onAnchorFound={ (e) => this._onAnchorFound(e,name)}
+          onAnchorUpdated={ (e) => this._onAnchorUpdated(e,name)}
+        />
+      )
+    });
+
+    return targets;
   }
 
   render() {
@@ -36,52 +71,46 @@ export default class HelloWorldSceneAR extends Component {
                   position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
 
         <ViroARImageMarker target={"targetOne"} >
-              <Globe 
-                satelliteClickCallback={this.props.arSceneNavigator.viroAppProps.satelliteClickCallback}
-                satelliteIDs={this.props.arSceneNavigator.viroAppProps.satelliteIDs}
-                timeScale={this.props.arSceneNavigator.viroAppProps.timeScale}
-              />
+          <Globe 
+            satelliteClickCallback={this.props.arSceneNavigator.viroAppProps.satelliteClickCallback}
+            satelliteIDs={this.props.arSceneNavigator.viroAppProps.satelliteIDs}
+            timeScale={this.props.arSceneNavigator.viroAppProps.timeScale}
+          />
         </ViroARImageMarker>
 
-        <ViroARImageMarker
-          target={"globeOne"}        
-          onAnchorFound={ (e) => this._onAnchorFound(e,"globeOne")}
-          onAnchorRemoved={ (e) => this._onAnchorRemoved(e,"globeOne")}
-        />
-
-        <ViroARImageMarker
-          target={"globeTwo"}        
-          onAnchorFound={ (e) => this._onAnchorFound(e,"globeTwo")}
-          onAnchorRemoved={ (e) => this._onAnchorRemoved(e,"globeTwo")}
-        />
-
-        <ViroARImageMarker
-          target={"globeThree"}        
-          onAnchorFound={ (e) => this._onAnchorFound(e,"globeThree")}
-          onAnchorRemoved={ (e) => this._onAnchorRemoved(e,"globeThree")}
-        />
-
-        <ViroARImageMarker
-          target={"globeFour"}        
-          onAnchorFound={ (e) => this._onAnchorFound(e,"globeFour")}
-          onAnchorRemoved={ (e) => this._onAnchorRemoved(e,"globeFour")}
-        />
-
-
+        {this.renderTargets()}
       </ViroARScene>
     );
   }
 
   _onAnchorFound = (e, targetName) => {
-      console.log("ANCHOR FOUND");
-      console.log(e);
-      console.log(targetName);
+    // console.log("ANCHOR FOUND");
+    // console.log(e);
+    // console.log(targetName);
   }
 
-  _onAnchorRemoved = (e, targetName) => {
-    console.log("ANCHOR REMOVED");
-    console.log(e);
-    console.log(targetName);
+  _onAnchorUpdated = (e, targetName) => {
+      if (e.trackingMethod === "tracking") {
+        // add target name to tracking if not there
+        if (!this.tracking.includes(targetName)) {
+          this.tracking = [...this.tracking, targetName];
+        }
+
+        // remove target name from lastKnownPoses
+        if (this.lastKnownPose.includes(targetName)) {
+          this.lastKnownPose = this.lastKnownPose.filter(e => e !== targetName);
+        }
+      } else if (e.trackingMethod === "lastKnownPose") {
+        // add to lastKnownPoses if not there
+        if (!this.lastKnownPose.includes(targetName)) {
+          this.lastKnownPose = [...this.lastKnownPose, targetName];
+        }
+
+        // remove from tracking
+        if (this.tracking.includes(targetName)) {
+          this.tracking = this.tracking.filter(e => e !== targetName);
+        }
+      }
   }
 
   _onInitialized(state, reason) {
@@ -100,36 +129,51 @@ ViroARTrackingTargets.createTargets({
     source : require('./res/targets/earthFlat.png'),
     orientation : "Up",
     physicalWidth : 0.2 // real world width in meters
-  },
-  "globeOne" : {
-    source : require('./res/targets/1_edited_opt.png'),
+  },  
+  "africa" : {
+    source : require('./res/targets/africa.jpg'),
     orientation : "Up",
-    physicalWidth : 0.3 // real world width in meters
+    physicalWidth : 0.3,
   },
-  "globeTwo" : {
-    source : require('./res/targets/2_edited_opt.png'),
+  "atlantic" : {
+    source : require('./res/targets/atlantic.jpg'),
     orientation : "Up",
-    physicalWidth : 0.3 // real world width in meters
+    physicalWidth : 0.3,
   },
-  "globeThree" : {
-    source : require('./res/targets/3_edited_opt.png'),
+  "australia" : {
+    source : require('./res/targets/australia.jpg'),
     orientation : "Up",
-    physicalWidth : 0.3 // real world width in meters
+    physicalWidth : 0.3,
   },
-  "globeFour" : {
-    source : require('./res/targets/4_edited_opt.png'),
+  "china" : {
+    source : require('./res/targets/china.jpg'),
     orientation : "Up",
-    physicalWidth : 0.3 // real world width in meters
+    physicalWidth : 0.3,
   },
-});
-
-var styles = StyleSheet.create({
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',  
+  "europe" : {
+    source : require('./res/targets/europe.jpg'),
+    orientation : "Up",
+    physicalWidth : 0.3,
+  },
+  "hawaii" : {
+    source : require('./res/targets/hawaii.jpg'),
+    orientation : "Up",
+    physicalWidth : 0.3,
+  },
+  "indonesia" : {
+    source : require('./res/targets/indonesia.jpg'),
+    orientation : "Up",
+    physicalWidth : 0.3,
+  },
+  "northAmerica" : {
+    source : require('./res/targets/northAmerica.jpg'),
+    orientation : "Up",
+    physicalWidth : 0.3,
+  },
+  "southAmerica" : {
+    source : require('./res/targets/southAmerica.jpg'),
+    orientation : "Up",
+    physicalWidth : 0.3,
   },
 });
 
