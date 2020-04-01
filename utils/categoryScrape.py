2a920c4b5e0468dd86abcdbaf9249a208270096f
pospisil98 <pospivo1@fel.cvut.cz>
2020-04-01 17:55:27 +0200

    Updated categories for select

--- utils/categoryScrape.py
+++ utils/categoryScrape.py
@@ -1,30 +1,113 @@
 import urllib.request
 import json
+import os
 
-savePath = "../js/res/selectCategories.json"
+savePath = "C:/Users/Vojcek/Desktop/BP/CODES/satviz/js/res/selectCategories.json"
 
 websites = [
-    "https://www.celestrak.com/NORAD/elements/gps-ops.txt",
+    "https://www.celestrak.com/NORAD/elements/stations.txt",
     "https://www.celestrak.com/NORAD/elements/weather.txt",
-    "https://www.celestrak.com/NORAD/elements/starlink.txt",
+    "https://www.celestrak.com/NORAD/elements/noaa.txt",
+    "https://www.celestrak.com/NORAD/elements/goes.txt",
+    "https://www.celestrak.com/NORAD/elements/resource.txt",
+    "https://www.celestrak.com/NORAD/elements/sarsat.txt",
+    "https://www.celestrak.com/NORAD/elements/dmc.txt",
+    "https://www.celestrak.com/NORAD/elements/tdrss.txt",
+    "https://www.celestrak.com/NORAD/elements/argos.txt",
+
+    "https://www.celestrak.com/NORAD/elements/intelsat.txt",
+    "https://www.celestrak.com/NORAD/elements/ses.txt",
+    "https://www.celestrak.com/NORAD/elements/iridium.txt",
+    "https://www.celestrak.com/NORAD/elements/iridium-NEXT.txt",
+    "https://www.celestrak.com/NORAD/elements/oneweb.txt",
+    "https://www.celestrak.com/NORAD/elements/orbcomm.txt",
+    "https://www.celestrak.com/NORAD/elements/globalstar.txt",
+    "https://www.celestrak.com/NORAD/elements/amateur.txt",
+    "https://www.celestrak.com/NORAD/elements/x-comm.txt",
+    "https://www.celestrak.com/NORAD/elements/other-comm.txt",
+    "https://www.celestrak.com/NORAD/elements/gorizont.txt",
+    "https://www.celestrak.com/NORAD/elements/raduga.txt",
+    "https://www.celestrak.com/NORAD/elements/molniya.txt",
+
+    "https://www.celestrak.com/NORAD/elements/gps-ops.txt",
+    "https://www.celestrak.com/NORAD/elements/glo-ops.txt",
+    "https://www.celestrak.com/NORAD/elements/galileo.txt",
+    "https://www.celestrak.com/NORAD/elements/beidou.txt",
+    "https://www.celestrak.com/NORAD/elements/sbas.txt",
+    "https://www.celestrak.com/NORAD/elements/nnss.txt",
+    "https://www.celestrak.com/NORAD/elements/musson.txt",
+
+    "https://www.celestrak.com/NORAD/elements/science.txt",
+    "https://www.celestrak.com/NORAD/elements/geodetic.txt",
+    "https://www.celestrak.com/NORAD/elements/engineering.txt",
+    "https://www.celestrak.com/NORAD/elements/education.txt",
+
+    "https://www.celestrak.com/NORAD/elements/military.txt",
+    "https://www.celestrak.com/NORAD/elements/radar.txt",
+    "https://www.celestrak.com/NORAD/elements/cubesat.txt",
+    "https://www.celestrak.com/NORAD/elements/other.txt",
 ]
 
 names = [
-    "GPS OPERATIONAL",
-    "WEATHER",
-    "STARLINK"
+    "Space Stations",
+    "Weather",
+    "NOAA",
+    "GOES",
+    "Earth Resources",
+    "Search & Rescue (SARSAT)",
+    "Disaster Monitoring",
+    "TDRSS",
+    "ARGOS",
+
+    "INTELSAT",
+    "SES",
+    "IRIDIUM",
+    "IRIDIUM-NEXT",
+    "ONEWEB",
+    "Orbcomm",
+    "Globalstar",
+    "Amateur Radio",
+    "Experimental",
+    "Other communication",
+    "Gorizont",
+    "Raduga",
+    "Molniya",
+
+    "GPS Operational",
+    "GLONASS Operational",
+    "Gallileo",
+    "Beidou",
+    "WAAS/EGNOS/MSAS",
+    "NNSS",
+    "Russian LEO Navigation",
+
+    "Space & Earth Science",
+    "Geodetic",
+    "Engineering",
+    "Education",
+
+    "Millitary misc",
+    "Radar Calibration",
+    "Cubesat",
+    "Other",
 ]
 
-parent_ids = [
-    1,
-    2,
-    3
-]
+print("Web count: " + str(len(websites)))
+print("Name count: " + str(len(names)))
+linecounttotal = 0
+
+parent_ids = []
 
 select = []
 
+for i in range(len(websites)):
+    parent_ids.append(i)
 
 for i in range(len(websites)):
+    print("Processing :" + websites[i])
+    print("Name: " + names[i])
+    print("")
+
     data = urllib.request.urlopen(websites[i])
     lineCounter = 0
 
@@ -46,12 +129,14 @@ for i in range(len(websites)):
             satid = decoded[2:7]
         elif lineCounter == 2:
             children.append({
-                'name': name,
+                'name': name + " ID:" + satid,
                 'id': satid,
             })
 
         lineCounter += 1
         lineCounter %= 3
+
+        linecounttotal += 1
 
     # sort by category name
     children = sorted(children, key= lambda i: i['name'])
@@ -62,5 +147,7 @@ for i in range(len(websites)):
 # sort by parent name
 select = sorted(select, key= lambda i: i['name'])
 
-with open(savePath, 'w') as out:
-    json.dump(select, out)
+print("Total lines " + str(linecounttotal))
+
+with open(savePath, 'w', encoding='utf-8') as f:
+    json.dump(select, f, ensure_ascii=False, indent=4)
